3998b32eb92321fdba2b6b3237fbcbdee1fb9560
Gareth Davis <gareth.davis@mac.com>
2011-08-23 20:38:52 +0200

    fixing typo

--- src/com/lps/numbers.clj
+++ src/com/lps/numbers.clj
@@ -14,7 +14,7 @@
       (triplets (.substring s chunksize)))))))
 
 (def words
-  "Dictionary of words for the transalation of digits to words"
+  "Dictionary of words for the translation of digits to words"
   { 1 "one" 2 "two" 3 "three" 4 "four" 5 "five" 6 "six" 7 "seven" 8 "eight" 9 "nine" 10 "ten"
     11 "eleven" 12 "twelve" 13 "thirteen" 14 "fourteen" 15 "fifteen" 16 "sixteen" 17 "seventeen" 18 "eigthteen" 19 "nineteen" 20 "twenty"
     30 "thirty" 40 "forty" 50 "fifty" 60 "sixty" 70 "seventy" 80 "eighty" 90 "ninety"
