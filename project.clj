5ddfd025f48c616e810efbb0896343894cbfa503
Gareth Davis <gareth.davis@mac.com>
2011-09-04 20:50:28 +0100

    groovy fonts

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
     [[org.clojure/clojure "1.2.1"]
      [org.clojure/clojure-contrib "1.2.0"]
      [compojure "0.6.4"]
+     [hiccup "0.3.6"]
      [ring/ring-jetty-adapter "0.3.9"]]
   :dev-dependencies [[lein-ring "0.4.5"]]
   :ring {:handler com.lps.webapp/app})
